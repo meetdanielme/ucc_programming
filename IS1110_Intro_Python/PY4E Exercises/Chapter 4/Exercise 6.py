3eba10b069e0f3cb09d7c19b0b444a2c923657da
Daniel Marcinkowski <hey@meetdaniel.me>
2025-10-18 18:27:46 +0100

    Added exercises from PY4E, Chapter 4 (Functions)

---
+++ IS1110_Intro_Python/PY4E Exercises/Chapter 4/Exercise 6.py
@@ -0,0 +1,13 @@
+def computepay(hours, rate):
+    if hours > 40:
+        pay = (40 * rate) + ((hours - 40) * rate * 1.5)
+    else:
+        pay = hours * rate
+    return pay
+
+hours = float(input("Enter hours: "))
+rate = float(input("Enter rate per hour: "))
+
+pay = computepay(hours, rate)
+
+print("Gross salary:", pay)
